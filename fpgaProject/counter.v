8c7f421f04c0035e000c29923c03713e92081873
Thiago Pinto Pereira Sena <108939221+ThiagoPPSena@users.noreply.github.com>
2023-09-25 15:09:06 -0300

    ajuste comentários counter

--- fpgaProject/counter.v
+++ fpgaProject/counter.v
@@ -1,18 +1,18 @@
 module counter(clk, rst, count);
-	input clk;
-	output reg [15:0] count;
-	input rst;
+	input clk;			//clock
+	output reg [15:0] count;	//contador
+	input rst;			//reset
 	
 	
 	
 always @(posedge clk) begin
 	
-	if (rst == 1)
+	if (rst == 1) //reset = 1, reinicia a contagem
 		count <= 0;
-	else if (count == 16'b111111111111111)
+	else if (count == 16'b111111111111111) // se chegar a contagem no limite, reinicia a contagem
 		count <= 0;
 	else
-		count <= count + 1;
+		count <= count + 1; //contagem incrementada
 	
 end
 
