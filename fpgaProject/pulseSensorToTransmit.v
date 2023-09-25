b716235b80f2535403d30c5368de1d5f72a268ed
Thiago Pinto Pereira Sena <108939221+ThiagoPPSena@users.noreply.github.com>
2023-09-25 15:16:22 -0300

    ajuste comentários pulseSensor

--- fpgaProject/pulseSensorToTransmit.v
+++ fpgaProject/pulseSensorToTransmit.v
@@ -1,29 +1,29 @@
 module pulseSensorToTransmit(
-	input clk,
-	input pulseInitial,
-	output reg pulseFinal
+	input clk,							//clock
+	input pulseInitial,						//Pulso inicial
+	output reg pulseFinal						//Pulso final prolongado
 );
 	
-	reg state;										// Estado atual
+	reg state;							// Estado atual
 	reg [10:0] countPulse = 0; 					// Contador de pulsos
 	
-	localparam  									// Declaração dos estados
+	localparam  							// Declaração dos estados
 		WAIT_PULSE = 0,
 		SEND_PULSE = 1;
 
 	
 	always @(posedge clk) begin
 		case (state)
-			WAIT_PULSE: begin 					// Estado responsável por aguardar um pulso.
-				if(pulseInitial == 1) begin
+			WAIT_PULSE: begin 				// Estado responsável por aguardar um pulso.
+				if(pulseInitial == 1) begin		//Se chegou o pulso, muda para o próximo estado
 					state <= SEND_PULSE;
 				end
 			end
-			SEND_PULSE: begin 					// Estado para segurar o pulso por aproximadamente 9600bps.
+			SEND_PULSE: begin 				// Estado para segurar o pulso em 1Mhz por aproximadamente 2*9600bps.
 				countPulse <= countPulse + 1;
 				if(countPulse == 102) begin
 					countPulse <= 0;
-					state <= WAIT_PULSE;
+					state <= WAIT_PULSE;		//Retorna ao estado de espera
 				end
 			end
 		endcase
